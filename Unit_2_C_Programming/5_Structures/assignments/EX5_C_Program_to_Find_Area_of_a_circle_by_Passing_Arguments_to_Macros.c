db3568320d76fb1409ead9f299bcbfa11fcedda0
abdelrahman sayed <anakuwity7@gmail.com>
2023-07-24 18:33:10 +0300

    update

---
+++ Unit_2_C_Programming/5_Structures/assignments/EX5_C_Program_to_Find_Area_of_a_circle_by_Passing_Arguments_to_Macros.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#define PI 3.14
+int main(void) {
+	float r;
+do
+{
+printf("enter radius: ");
+scanf("%f",&r);
+printf("area = %lf\n",PI*r*r);
+
+}
+while(1);
+}
